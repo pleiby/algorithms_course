58d6c3618f983a13edb31931db86a3656627f229
Paul Leiby <pleiby@gmail.com>
2020-05-04 12:59:13 -0400

    add simple annotations to original solution template

--- Course3_Algs_on_Graphs/week1_decomposition1/1_reachability/reachability.py
+++ Course3_Algs_on_Graphs/week1_decomposition1/1_reachability/reachability.py
@@ -4,16 +4,35 @@ import sys
 
 def reach(adj, x, y):
     #write your code here
+    print("Hello!\n")
+    print(adj)
     return 0
 
+"""
+Starts by reading a graph.
+Graphs are given as follows.
+The first line contains non-negative integers 𝑛 and 𝑚 
+ — the number of vertices and the number of edges respectively. 
+The vertices are always numbered from 1 to 𝑛. 
+Each of the following 𝑚 lines defines an edge in the format u v where
+1 ≤ 𝑢, 𝑣 ≤ 𝑛 are endpoints of the edge. 
+If the problem deals with an undirected graph this defines an undirected edge between 𝑢 and 𝑣. 
+In case of a directed graph this defines a directed edge from 𝑢 to 𝑣. 
+If the problem deals with a weighted graph 
+then each edge is given as u v w where 𝑢 and 𝑣 are vertices and 𝑤 is a weight.
+"""
 if __name__ == '__main__':
     input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
+    data = list(map(int, input.split())) # read graph description
+    n, m = data[0:2] # n vertices, m edges
     data = data[2:]
+    # create edge list (m ordered pairs)
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    # read the last ordered pair
     x, y = data[2 * m:]
+    # create adjacency list
     adj = [[] for _ in range(n)]
+    # renumber all vertices from 0 to n-1, easing pythonic indexing
     x, y = x - 1, y - 1
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
